af71cd005d215ece31e03d2b48a00ba2de13ce15
roisro35 <roisro35@gmail.com>
2021-08-06 23:34:41 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import "semantic-ui-css/semantic.min.css";
-import {Button} from "semantic-ui-react";
+import {Grid} from "semantic-ui-react";
 
 
 class App extends Component {
   render() {
     return(
       <div>
-        <h1>Selamat Datang React Semantic UI</h1>
-        <Button primary>Button Pertama</Button>
-        <Button secondary>Button Kedua</Button>
+        <Grid container columns={3}>
+          <Grid.Column>1</Grid.Column>
+          <Grid.Column>2</Grid.Column>
+          <Grid.Column>3</Grid.Column>
+        </Grid>
       </div>
     )
   };
